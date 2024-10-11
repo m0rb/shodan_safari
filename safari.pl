69034598640d1d6f7461d78c646ca51546c85009
morb <morb@misentropic.com>
2024-10-10 21:09:29 -0400

    Image quadrant filtering added. This should cut down on a greater portion of the blank screens or similar.

--- safari.pl
+++ safari.pl
@@ -11,13 +11,17 @@ use MyLib::ATProto    qw(post_media);
 use Image::Compare;
 use Mastodon::Client;
 
+
+START:
+
 my $wd     = "/home/morb/shodan_safari/";
+my $exe    = "/home/morb/shodan/bin/shodan";
 my $file   = "shodan-latest.json.gz";
 my $track  = "hosts.track";
-my $imgdir = $wd . substr( $file, 0, -8 ) . "-images/";
+my $imgdir = $wd . substr( $file, 0, -8 ) . "-images";
 my $cfg    = read_config_file("config.cfg");
 my $csv    = $wd . "shodan-latest.csv";
-my ( @previous, @hosts, $msg, $pass, $ok );
+my ( @previous, @hosts, $msg );
 
 my $mt = Mastodon::Client->new(
   instance        => $cfg->{masto}{instance},
@@ -47,11 +51,12 @@ my ( $ip, $port, $hostname, $city, $country, $asn, $isp, $timestamp, $id ) =
 if ( grep( /$ip-$port/, @previous ) ) { goto START; }
 
 my @prevbuf = grep(/$ip/, @previous );
+my ($pass,$ok) = (0,0);
+my $img = $imgdir."/$ip-$port\.jpg";
+
 foreach my $compare (@prevbuf) {
-  $cmp->set_image1( img => $imgdir . $compare."\.jpg", type => "jpeg" );
-  $cmp->set_image2( img => $imgdir . "$ip-$port\.jpg", type => "jpeg" );
-  $cmp->set_method( method => &Image::Compare::THRESHOLD_COUNT, arg => 300 );
-  my $return = $cmp->compare;
+  my $cmg = $imgdir."/".$compare."\.jpg";
+  my $return = imgcompare($cmg,$img,300);
   if ($return && $return < 350) {
     $pass++;
   } else {
@@ -59,25 +64,32 @@ foreach my $compare (@prevbuf) {
   }
 }
 
-if ($pass > $ok) { push(@previous,$ip."-".$port) and goto START; }
+if (imgcrop($img)) {
+  for(my $a=0;$a<4;$a++) {
+      my $b=$a+1; ($b>3) ? $b=0 : undef;
+    imgcompare("outbuf-$a.jpg","outbuf-$b.jpg",50) ? $ok++ : $pass++; 
+  }
+}
+
+if ( $pass > $ok ) { push(@previous,$ip."-".$port) and goto START; }
 
 if ( length $hostname >= 50 ) { $hostname = substr( $hostname,0,50 )."..."; }
 
 ($timestamp)          ? $timestamp = substr( $timestamp, 0, -10 ) : undef;
-($id)                 ? $msg .= "ID: $id\n"                   : undef;
-( $ip && $port )      ? $msg .= "IP: $ip:$port\n"             : undef;
-($hostname)           ? $msg .= "Hostname: $hostname\n"       : undef;
+#($id)                 ? $msg .= "ID: $id\n"                   : undef;
+#( $ip && $port )      ? $msg .= "IP: $ip:$port\n"             : undef;
+#($hostname)           ? $msg .= "Hostname: $hostname\n"       : undef;
 ($asn)                ? $msg .= "ASN: $asn\n"                 : undef;
-($isp)                ? $msg .= "ISP: $isp\n"                 : undef;
+#($isp)                ? $msg .= "ISP: $isp\n"                 : undef;
 ( $city && $country ) ? $msg .= "Location: $city, $country\n" : undef;
 ($timestamp)          ? $msg .= "Added: $timestamp\n"         : undef;
 $msg .= "\n#shodansafari #infosec";
 utf8::decode($msg);
 
-if ( -f $imgdir . "$ip-$port\.jpg" ) {
-  post_media( {account => "safari", text => $msg, fn => $imgdir . "$ip-$port\.jpg"} );
+if ( -f $img ) {
+  post_media( {account => "safari", text => $msg, fn => $img } );
   eval {
-    my $mm = $mt->upload_media( $imgdir . "$ip-$port\.jpg" );
+    my $mm = $mt->upload_media( $img );
     $mt->post_status( $msg, {media_ids => [ $mm->{'id'} ]} );
   };
 }
@@ -88,5 +100,32 @@ store( \@previous, $wd . $track );
 sub _setup {
   srand;
   unless ( -f $wd . $track ) { store( \@previous, $wd . $track ); }
-  unless ( -d $imgdir || -f $csv ) { `./update.sh` }
+  unless ( -f $wd . $file )  { die "No json bundle. Full stop.\n"; }
+  unless ( -d $imgdir )      { `$exe convert $file images` or die; }
+}
+
+sub imgcrop {
+  my @fileinfo = split(",\ ",`file @_`);
+  chomp @fileinfo;
+  my $res = $fileinfo[(scalar(@fileinfo) - 2)];
+  my ($x,$y) = split("x",$res);
+  unless($x%2||$y%2) {
+  my $seg = ($x/2)."x".($y/2);
+  `rm -f outbuf-?.jpg`;
+  `convert @_ -crop $seg\! outbuf.jpg`;
+  unless ( -f "outbuf-0.jpg" && -f "outbuf-3.jpg" ) {
+    die "bork\n";
+  }
+  } else {
+    return 0;
+  }
+}
+
+sub imgcompare {
+  my ($img1,$img2,$args) = @_;
+  my $cmp = Image::Compare->new;
+  $cmp->set_image1(img => $img1,type => 'jpeg');
+  $cmp->set_image2(img => $img2,type => 'jpeg');
+  $cmp->set_method(method => &Image::Compare::THRESHOLD_COUNT,args => $args);
+  return $cmp->compare;
 }
